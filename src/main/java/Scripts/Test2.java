3ac4e64e0ff67cc532842ad8d321fb06508f10a1
Dell <Dell@DESKTOP-GLBA18E>
2022-12-11 19:12:50 -0300

    hi

---
+++ src/main/java/Scripts/Test2.java
@@ -0,0 +1,11 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+ @Test
+ public void tc1() {
+ Reporter.log("Hello Selenium" , true);
+}
+}
