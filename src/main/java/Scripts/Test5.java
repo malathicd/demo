c8c0de7b7903bc66e8516de21d81eec78f3011fd
Dell <Dell@DESKTOP-GLBA18E>
2022-12-11 21:01:32 -0300

    hi-added

---
+++ src/main/java/Scripts/Test5.java
@@ -0,0 +1,11 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+ @Test
+ public void tc1() {
+ Reporter.log("Hello API" , true);
+}
+}
---
+++ src/main/java/Scripts/Test6.java
@@ -0,0 +1,11 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+ @Test
+ public void tc1() {
+ Reporter.log("Hello postman" , true);
+}
+}
